5d0503d7e4afe4c6c0978a1923a734ae3efff1e3
nayaib98 <nayaiburgess@gmail.com>
2018-12-14 14:23:18 -0500

    1st attempt to print a letter in the DOM

---
+++ app.js
@@ -0,0 +1,16 @@
+//Gather an array for my words
+
+var words = ['bike', 'General Assembly', 'Atlanta', 'Falcons', 'Hawks', 'Coca-Cola']
+//HOW TO CREATE DASHES
+// var answer =[]
+// for(x = 0; x < words.length; x++)
+//     answer[x] = '_';
+
+ function inputVal(){
+     letters = document.querySelector("#keyboard.button")
+        // guess = document.createElement("li");
+        // guess.appendChild(letters.value);
+        
+        console.log(letters)
+ }
+ 

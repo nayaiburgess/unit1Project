3581e01517094a692e4b63e3d13c675b4c95896f
nayaib98 <nayaiburgess@gmail.com>
2018-12-17 15:31:19 -0500

    creating function for random words/ and attempting dashes

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 //Gather an array for my words
 
-var words = ['bike', 'General Assembly', 'Atlanta', 'Falcons', 'Hawks', 'Coca-Cola']
+var words = ['bike', 'General Assembly', 'Atlanta', 'Falcons', 'Hawks', 'Coca-Cola', 'Aquarium', 'Zoo', 'Cascade']
+currentWord = words[Math.floor(Math.random() * words.length)];
+var word = document.createElement("div")
+    var guessWord = document.createTextNode(currentWord)
+    word.appendChild(guessWord)
+    document.getElementById('guessSpace').appendChild(word);
+
 //HOW TO CREATE DASHES
-// var answer =[]
+// var answer = []
 // for(x = 0; x < words.length; x++)
 //     answer[x] = '_';
 
-// PASS IN THE OBJECT BUTTON
+
+
+// PASS IN THE OBJECT 'BUTTON' IN THE FUNCTION
 function currentValue(button){
 // GRAB THE VALUE OF THE OBJECT
-var currentLetter = button.value
-console.log(currentLetter)
-  var x = document.createElement("li")
-  var t = document.createTextNode(currentLetter)
-  x.appendChild(t)
- document.getElementById('letterspace').appendChild(x);
+    var currentLetter = button.value
+    var x = document.createElement("li")
+    var t = document.createTextNode(currentLetter)
+    x.appendChild(t)
+    document.getElementById('letterspace').appendChild(x);
 
 }
 

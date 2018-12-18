c6643ab8ce0f6348628c089854f8c2356d0c6bb6
nayaib98 <nayaiburgess@gmail.com>
2018-12-18 12:39:13 -0500

    pictures now keep track of incorrect answers, alerts for win/loss

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //Gather an array for my words
+let incorrectAnswer = 1;
 
-var words = ['BIKE', 'ATLANTA', 'FALCONS', 'HAWKS', 'COLA', 'AQUARIUM', 'ZOO', 'CASCADE']
+
+var words = ['RED', 'BLUE', 'ORANGE', 'BLACK', 'BROWN', 'PINK', 'WHITE', 'TAN', 'GRAY','SILVER', 'GOLD']
 currentWord = words[Math.floor(Math.random() * words.length)];
 guess = currentWord.split("")
 console.log(guess)
@@ -17,10 +19,6 @@ for( y = 0; y < guess.length; y++){
 }
 document.getElementById('guessSpace').innerHTML = emptyArray.join(' ');
 
- // if (currentLetter == guess[y]){
-    //     guess[y] == currentLetter;
-    //     console.log(currentLetter);
-    // }
 
 
 // PASS IN THE OBJECT 'BUTTON' IN THE FUNCTION
@@ -30,17 +28,40 @@ function currentValue(button){
     //THIS FINDS OUT WHERE IT IS IN THE ARRAY
     let index = guess.indexOf(currentLetter);
     //THIS REPLACES THE DASH OF THE POSITION IN THE ARRAY
+    
     emptyArray[index] = currentLetter;
     // THIS PUTS THE ARRAY IN THE DOM
-    document.getElementById('guessSpace').innerHTML = emptyArray.join(' ');
+        document.getElementById('guessSpace').innerHTML = emptyArray.join(' ');
+        //ALERT FOR WIN
+        if(emptyArray.join('') === guess.join('')){
+            alert("You have won")
+            
+        }
+        
 
-    // if (currentLetter == guess[attempt]){
-    //     console.log(guess[attempt]);
-    // }
-    // else{
-    //      alert("not working")
-    //      return false;
-    // }
-
-    // }       
-}
+    if(currentLetter !== guess[index]){
+        if (incorrectAnswer == 1){
+            document.getElementById('man').style.visibility = 'visible'
+            incorrectAnswer++
+            }
+        else if(incorrectAnswer == 2){
+            document.getElementById('body').style.visibility = 'visible'
+            incorrectAnswer++
+        }
+        else if(incorrectAnswer == 3){
+            document.getElementById('leftarm').style.visibility = 'visible'
+            incorrectAnswer++
+        }
+        else if(incorrectAnswer == 4){
+            document.getElementById('rightarm').style.visibility = 'visible'
+            incorrectAnswer++
+        }
+        else if(incorrectAnswer == 5){
+            document.getElementById('leftleg').style.visibility = 'visible'
+            incorrectAnswer++
+        }
+        else{
+            document.getElementById('rightleg').style.visibility = 'visible'
+            alert("Game over")
+        }
+    }}
